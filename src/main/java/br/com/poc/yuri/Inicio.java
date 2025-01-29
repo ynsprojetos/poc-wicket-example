751c72da1a9e6de2026eaab32e2d033365c2cf95
Yuri <ynscontato@gmail.com>
2025-01-28 23:24:03 -0300

    Criando uma home page

---
+++ src/main/java/br/com/poc/yuri/Inicio.java
@@ -0,0 +1,17 @@
+package br.com.poc.yuri;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
+public class Inicio extends WebPage {
+
+    private static final long serialVersionUID = 1234567890L;
+
+    public Inicio(){
+        Label labelMensagemBoasVindas = new Label("mensagemBoasVindas", Model.of("Bem vindo à agenda eletronica!"));
+        add(labelMensagemBoasVindas);
+    }
+
+
+}
--- src/main/java/br/com/poc/yuri/WicketApplication.java
+++ src/main/java/br/com/poc/yuri/WicketApplication.java
@@ -19,7 +19,7 @@ public class WicketApplication extends WebApplication
 	@Override
 	public Class<? extends WebPage> getHomePage()
 	{
-		return HomePage.class;
+		return Inicio.class;
 	}
 
 	/**
